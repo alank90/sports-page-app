14918d14a87f7380c0f3ed5e489ed8a96a30f443
akillian@outlook.com <akillian@outlook.com>
2018-08-02 20:28:04 -0400

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,11 @@
-// DOM is ready
-$(document).ready(function () {
-    let moreLink = document.createElement("a");
-    let lessLink = document.createElement("a");
-    let allParagraphs = document.getElementsByTagName("p");
-    let firstParagraph = allParagraphs[0];
+//src/js/main.js
+const Vue = require('vue');
 
-    moreLink.setAttribute("href", "#");
-    moreLink.setAttribute("class", "more-link");
-    moreLink.innerHTML = "Read more";
-
-    firstParagraph.appendChild(moreLink);
-
-    for (let p of allParagraphs) {
-        if (p != allParagraphs[0]) {
-            p.style.display = "none";
-        }
+const app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Get the Latest Sports Scores and Standings'
     }
+  });
 
-
-    // ============ Routine to reveal hidden paragraphs ======== //
-
-    $(".more-link").on("click", function () {
-        let lastParagraph = allParagraphs[allParagraphs.length - 1];
-        moreLink.style.display = "none";
-
-        for (let p of allParagraphs) {
-            if (p != allParagraphs[0]) {
-                p.style.display = "block";
-            }
-        }
-
-        lessLink.setAttribute("href", "#");
-        lessLink.setAttribute("class", "less-link");
-        lessLink.innerHTML = "Less";
-        lastParagraph.appendChild(lessLink);
-
-        if (lessLink.style.display === 'none') {
-            lessLink.style.display = "inline-block";
-        }
-
-    });
-
-
-    // ====== Routine to hide paragraphs ============= //
-    // note: we had to handle this with event delegation ==== //
-
-    $("p").on("click", ".less-link", function () {
-        console.log("Clicked less-link button");
-        lessLink.style.display = "none";
-
-        for (let p of allParagraphs) {
-            if (p != allParagraphs[0]) {
-                p.style.display = "none";
-            }
-        }
-
-        moreLink.style.display = "inline-block";
-    });
-
-
-}); // end dom ready
+  module.exports = app;
