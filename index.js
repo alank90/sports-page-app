cfb195203b4906bc7dcfb8004d2c1c2f7a823b3d
akillian@outlook.com <akillian@outlook.com>
2018-08-08 20:00:33 -0400

    commit

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
 // ./index.js includes
-// require("./src/js/vue");
+require("./src/js/vue");
 
 // if you want to add more js libs, just put the require statements here
-const Vue = require("vue");
-const axios = require("axios");
-
-// Axios config object. Sent with get request
-const config = {
-  // `headers` are custom headers to be sent
-  headers: {
-    Authorization:
-      "Basic NzAxMzNkMmEtNzVmMi00MjdiLWI5ZDYtOTgyZTFhOnNwb3J0c2ZlZWRzMjAxOA=="
-  },
-  // `params` are the URL parameters to be sent with the request
-  // Must be a plain object or a URLSearchParams object
-  params: {
-    team: "nyy",
-    force: true
-  }
-};
-
-new Vue({
-  el: "#app",
-  data() {
-    return {
-      info: null
-    };
-  },
-  mounted() {
-    // Use Axios Get to retrieve the baseball info
-    axios
-      .get(
-        "https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/scoreboard.json?fordate=20180804",
-        config
-      )
-      .then(response => {
-        this.info = response;
-      });
-  }
-});
--- src/js/vue.js
+++ src/js/vue.js
@@ -1,11 +1,38 @@
 //src/js/vue.js
-const Vue = require('vue');
+const Vue = require("vue");
+const axios = require("axios");
 
-const app = new Vue({
-    el: '#app',
-    data : {
-      info: info
-    }
+// Axios config object. Sent with get request
+const config = {
+  // `headers` are custom headers to be sent
+  headers: {
+    Authorization:
+      "Basic NzAxMzNkMmEtNzVmMi00MjdiLWI5ZDYtOTgyZTFhOnNwb3J0c2ZlZWRzMjAxOA=="
+  },
+  // `params` are the URL parameters to be sent with the request
+  // Must be a plain object or a URLSearchParams object
+  params: {
+    team: "nyy",
+    force: true
+  }
+};
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    // Use Axios Get to retrieve the baseball info
+    axios
+      .get(
+        "https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/scoreboard.json?fordate=20180804",
+        config
+      )
+      .then(response => {
+        this.info = response;
+      });
+  }
 });
-  
-  module.exports = app;
