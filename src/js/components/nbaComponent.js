5c65e48abd2ec81bbd493d18076d9dbe1e0754af
Alan K <akillian@outlook.com>
2018-09-14 14:22:01 -0400

    Add NFL Standings Component

---
+++ src/js/components/nbaComponent.js
@@ -0,0 +1,12 @@
+// src/js/components/nbaComponent.js
+
+
+const Vue = require("vue");
+
+const nbaTemplate = function() {
+  Vue.component("tab-nba", {
+    template: "<div>NBA component</div>"
+  });
+};
+
+module.exports = nbaTemplate;
