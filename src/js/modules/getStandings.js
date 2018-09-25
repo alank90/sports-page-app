bce29c52902dec37f46c8ce6d7986b954547fe47
Alan K <akillian@outlook.com>
2018-09-25 14:04:54 -0400

    commit more refactorint getStandings into seperate module

---
+++ src/js/modules/getStandings.js
@@ -0,0 +1,12 @@
+// ====================================================================== //
+//====== Get Standings From MySportsFeeds Site ========================== //
+// ===================================================================== //
+
+const mySportsFeeds = require("./mySportsFeeds");
+
+/* jshint ignore:start */
+module.exports = async url => {
+  standings = await mySportsFeeds.feedsData(url);
+  return standings;
+};
+/* jshint ignore:end */
