29b26254fa15f70dd217b5b65b508298b3743c7d
Alan K <akillian@outlook.com>
2018-10-03 13:06:51 -0400

    Excluding MiscOperations Function

--- test.js
+++ test.js
@@ -1,48 +1,40 @@
-// ====== Read data from dist/index.html(getData) =============== //
+// ================ Start MiscOperations =============================== //
 
       /* jshint ignore:start */
-      const getData = async function(result) {
+      const miscOperations = async function(result) {
         console.log(result);
 
-        let distIndexHtml;
-        // Lets update dist/index.html file src and href links to reflect new location
-        console.log(
-          "index.html: Redoing file links to reflect move to /dist folder."
-        );
-        fs.readFile("dist/index.html", "utf8", function(err, data) {
-          if (err) {
-            console.log(err);
-          }
+        try {
+          // Copy CNAME to /dist folder
+          await checkFileAccess("CNAME", fs.constants.R_OK | fs.constants.W_OK);
+          await fc("CNAME", "dist/CNAME");
 
-          // check and replace both src= and href= links to reflect chenge to dist/ folder
-          // Notice we chained .replace to do it
-          const regEx1 = /src\s*=\s*"\.\/src\//gi;
-          const regEx2 = /src\s*=\s*'\.\/src\//gi;
-          const regEx3 = /href\s*=\s*"\.\/src\//gi;
-          const regEx4 = /href\s*=\s*'\.\/src\//gi;
-
-          distIndexHtml = data
-            .replace(regEx1, 'src="./')
-            .replace(regEx2, "src='./")
-            .replace(regEx3, 'href="./')
-            .replace(regEx4, "href='./");
-
-          fs.stat("./dist/index.html", function(err, stats) {
+          // Copy /src/resources to /dist folder
+          fs.readdir("./resources", (err, files) => {
             if (err) {
-              console.log(`Error: ${err}`);
-            } else if (stats.size === 0) {
-              console.log(`Error copying index.html!!!!!!`);
+              console.log("Error reading resources directory!");
+            } else if (!files.length) {
+              console.log("resources directory empty");
             } else {
-              console.log(
-                `Succesfully copied to dist\index.html. File size is ${
-                  stats.size
-                }`
-              );
+              console.log("resources directory present. Copying to /dist.");
+              copydir("resources", "dist/resources", err => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log("Copied /resources directory ok");
+                }
+              });
             }
-          });
-        }); // end fs.readfile index.html
+          }); // end fs.readdir
 
-        return distIndexHtml;
-      }; //  End async function
-      // ============ End getData function ================= //
+          setTimeout(function() {
+           console.log("Build Process Completed...");
+          }, 1500);
+
+          return "Test";
+        } catch (err) {
+          return console.log("ERROR:", err);
+        }
+      };
       /* jshint ignore:end */
+      // ================= End MiscOperations ================================ //
