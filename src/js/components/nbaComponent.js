43ea9fa6e8300d8396ad20a673c7cbb8abd02def
Alan K <akillian@outlook.com>
2018-10-09 14:35:40 -0400

    commit add nba component

--- src/js/components/nbaComponent.js
+++ src/js/components/nbaComponent.js
@@ -1,11 +1,96 @@
 // src/js/components/nbaComponent.js
 
-
 const Vue = require("vue");
 
 const nba = {
   nbaComponent: Vue.component("tab-nba", {
-    template: "<div>NBA component</div>"
+    props: [
+      "props_league_data",
+      "props_league_standings",
+      "props_basketball_playoffs"
+    ],
+    template: `
+                <div class="vue-root-element">
+                    <div class="container mlb-scores">
+                        <div class="row">
+                            <div class="col-xs-12 col-md-4 col-lg-3" v-for="arrayItem in props_league_data">
+                                <table class="table table-striped table-sm">
+                                        <thead>
+                                            <th scope="col" class="box-score-status is-completed" v-if="arrayItem.isCompleted">Final</th>
+                                        </thead>
+                    
+                                        <tbody>
+                                            <tr>
+                                              <td class="team"> {{ arrayItem.game.awayTeam.Abbreviation }} </td>
+                                              
+                                              <td v-if="arrayItem.isCompleted === 'false'">Not Completed/Postponed: {{arrayItem.game.delayedOrPostponedReason}}</td>
+                                              <template v-else>
+                                                  <td class="inning-or-quarter-score" v-for="quarter_score in arrayItem.quarterSummary.quarter">
+                                                      {{quarter_score.awayScore }}
+                                                  </td>
+                                                  <td class="box-score-final" v-bind:class="{ won: Number(arrayItem.awayScore) > Number(arrayItem.homeScore)}">{{ arrayItem.awayScore }}
+                                                  </td>
+                                              </template>
+
+                                              <td v-if="arrayItem.game.awayTeam.Abbreviation === 'NYK'"><img class="team-logo" scope="row"  src="./src/img/nyk.png"></td>
+                                              <td v-if="arrayItem.game.awayTeam.Abbreviation === 'BRO'"><img class="team-logo" scope="row" src="./src/img/bro.png"></td>
+                                            </tr>
+
+                                            <tr>
+                                            <td class="team"> {{ arrayItem.game.homeTeam.Abbreviation }} </td>
+
+                                            <td v-if="arrayItem.isCompleted === 'false'"></td>
+                                            <template v-else>
+                                                <td class="inning-or-quarter-score" v-for="quarter_score in arrayItem.quarterSummary.quarter">
+                                                    {{quarter_score.homeScore }}</td>
+                                                <td class="box-score-final" v-bind:class="{ won: Number(arrayItem.homeScore) > Number(arrayItem.awayScore)}">{{ arrayItem.homeScore }}
+                                                </td>
+                                            </template>
+                                            <td v-if="arrayItem.game.homeTeam.Abbreviation === 'NYK'"><img class="team-logo" scope="row" src="./src/img/nyk.png"></td>
+                                            <td v-if="arrayItem.game.homeTeam.Abbreviation === 'BRO'"><img class="team-logo" scope="row" src="./src/img/bro.png"></td>
+                                        </tr>
+                                        <tr><td class="team w-150 location">Location:  {{ arrayItem.game.location }} </td></tr>
+                                            
+                                        </tbody>
+                                    </table>
+                            </div> <!-- End v-for -->
+                            
+                        </div> <!-- End of row -->
+                    </div> <!-- End container -->
+
+                    <hr>
+                    <div class="container nfl-standings">
+                        <div class="row">
+                            <div class="col-12 col-md-4 division-name text-center" v-for="value in props_league_standings">
+                                {{ value['@name'] }} 
+                                <div class="team" v-for="item in value.teamentry">
+                                    <!-- <p> {{ item }} </p> -->
+                                    <table class="table table-striped table-sm">
+                                        <thead>
+                                            <th scope="col"></th>
+                                            <th scope="col">{{ item.stats.Wins['@abbreviation'] }}</th>
+                                            <th scope="col">{{ item.stats.Losses['@abbreviation'] }}</th>
+                                            <th scope="col">{{ item.stats.GamesBack['@abbreviation'] }}</th>
+                                            <th scope="col">{{ item.stats.WinPct['@abbreviation'] }} </th>
+                                        </thead> 
+                                        <tbody>
+                                        <tr>
+                                            <td class="team">{{ item.team.Abbreviation }}</td>
+                                            <td class="team">{{ item.stats.Wins['#text'] }}</td>
+                                            <td class="team">{{ item.stats.Losses['#text'] }}</td>
+                                            <td class="team" v-if="item.stats.GamesBack['#text'] === '0.0'"> -- </td>
+                                            <td class="team" v-else>{{ item.stats.GamesBack['#text'] }}</td>
+                                            <td v-if="item.stats.Losses['#text'] != '0'" class="team">{{ item.stats.WinPct['#text'].slice(1) }}</td>
+                                            <td v-else class="team">100%</td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                                </div> <!-- End item in value.teamentry -->
+                            </div> <!-- end v-for in props_league_standings -->                   
+                        </div>  <!-- End row -->
+                    </div> <!-- End container -->
+                  </div> <!-- End Vue root -->
+                ` // End Template Literal
   })
 };
 
--- src/js/vue.js
+++ src/js/vue.js
@@ -54,14 +54,13 @@ new Vue({
   el: "#app",
   data() {
     return {
-      baseball_feeds_data: [],
+      sports_feeds_data: [],
       baseball_playoffs: false,
       nfl_feeds: {
         sunday_data: [],
         thurs_data: [],
         mon_data: []
       },
-      nba_feeds_data: [],
       standings: [],
       currentTab: "",
       sport_logo_image: "",
@@ -99,7 +98,9 @@ new Vue({
 
         // ========================================================================= //
         // ============ First Get the  MLB Sports Scores =========================== //
-        // Check if it's the Regular or Post Season
+        // ========================================================================= //
+
+        // Check if it's the Regular or Post Season ===================== //
         if (date.yesterday < `${date.year}1001`) {
           seasonName = `${date.year}-regular`;
         } else if (
@@ -120,7 +121,7 @@ new Vue({
             config
           )
           .then(response => {
-            this.baseball_feeds_data = response.data.scoreboard.gameScore;
+            this.sports_feeds_data = response.data.scoreboard.gameScore;
           })
           .catch(error => {
             console.log(error);
@@ -130,6 +131,7 @@ new Vue({
         // End ==== get.then ====== //
         // ================================================================================= //
         // ============================ End Get MLB Scores ================================= //
+        // ================================================================================= //
 
         // ================================================================================== //
         // =========================== Get MLB Standings ==================================== //
@@ -226,15 +228,92 @@ new Vue({
         // ================= End Get NFL Standings ===================== //
         // ============================================================= //
 
-        // ============================================================================= //
-        // ================== else check if the NBA ==================================== //
-        // ============================================================================= //
+        // ============================================================== //
+        // ================== else check if the NBA ===================== //
+        // ============================================================== //
       } else if (this.currentTab === "NBA") {
-        console.log("Call NBA API");
+        this.loading = true;
+        this.sport_logo_image = "./src/img/" + this.currentTab + ".png";
+        let startOfSeasonYear = "";
+        config.params = {
+          force: true
+        };
+        // ========================================================================= //
+        // ============ Get the NBA Sports Scores ================================== //
+        // ========================================================================= //
+
+        // Check if it's the Regular or Post Season ================= //
+        if (date.yesterday > `${date.year}1007`) {
+          // Note: Change startOfSeasonYear on every new season !!!!!!!!! ======= //
+          startOfSeasonYear = `2018`;
+          seasonName = `${startOfSeasonYear}-${startOfSeasonYear + 1}-regular`;
+        } else if (
+          date.yesterday > `${date.year}0412` &&
+          date.yesterday < `${date.year}0607`
+        ) {
+          seasonName = `${startOfSeasonYear}-playoff`;
+          config.params = "";
+        } else {
+          console.log("End of Basketball Season. See you next year!");
+        }
+
+        axios
+          .get(
+            `https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2018-regular/scoreboard.json?fordate=20180321`,
+            config
+          )
+          .then(response => {
+            this.sports_feeds_data = response.data.scoreboard.gameScore;
+          })
+          .catch(error => {
+            console.log(error);
+            this.errored = true;
+          })
+          .finally(() => (this.loading = false));
+        // End ==== get.then ====== //
+        // ================================================================================= //
+        // ============================ End Get NBA Scores ================================= //
+        // ================================================================================= //
+
+        // ================================================================================== //
+        // =========================== Get NBA Standings ==================================== //
+        // ================================================================================== //
+        // Check if it's the Regular or Post Season
+        if (date.yesterday > `${date.year}1007`) {
+          // Note: Change startOfSeasonYear on every new season !!!!!!!!! ======= //
+          startOfSeasonYear = `2018`;
+          seasonName = `${startOfSeasonYear}-${startOfSeasonYear + 1}-regular`;
+          teamStats = `W,L,GB,Win %`;
+          typeOfStandings = "division_team_standings";
+        } else if (
+          date.yesterday > `${date.year}0412` &&
+          date.yesterday < `${date.year}0607`
+        ) {
+          seasonName = `${date.year}-playoff`;
+          this.baseball_playoffs = true;
+          teamStats = `W,L`;
+          typeOfStandings = "division_team_standings";
+          config.params = "";
+        } else {
+          console.log("End of Basketball Season. See you next year!");
+        }
+        url = `https://api.mysportsfeeds.com/v1.2/pull/nba/2017-2018-regular/division_team_standings.json`;
+        /* jshint ignore:start */
+        // Note: We use the arrow function here because "this" is defined by where
+        // getStandings() is called (the vue instance) not by where it is used.
+        leagueStandings = async () => {
+          this.standings = await getStandings(url);
+        };
+        /* jshint ignore:end */
+        leagueStandings();
+        // =============================================================================== //
+        // =========================== End NBA Standings ================================= //
+        // =============================================================================== //
       }
-    } // === end getSportsData ===== //
+    } // ======= end getSportsData =============== //
   }
 });
-// ========================================================= //
-// ========== End Vue Instance ============================= //
-// ========================================================= //
+
+// ================================================================================================== //
+// ====================================== End Vue Instance ========================================== //
+// ================================================================================================== //
