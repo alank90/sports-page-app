5adc8cff26405cf501a8332c31264a3b860cfafb
Alan K <akillian@outlook.com>
2019-08-15 20:44:20 -0400

    commit

--- src/js/components/playerCumlativeStats.js
+++ src/js/components/playerCumlativeStats.js
@@ -5,14 +5,36 @@ const playerCumlativeStats = {
   playerCumlativeStats: Vue.component("mlb-player-stats", {
     data() {
       return {
-        boxGameScore: null
+        currentPlayerId: ""
       };
     },
     mounted() {
-      EventBus.$on("boxGameScoreObject", data => {
-        // Assign data on eventBus to this component object
-        this.boxGameScore = data;
+      EventBus.$on("get-cumlative-stats", data => {
+        // Assign playerid on eventBus to this component object
+        // and have axios get player cumlative stats
+        this.currentPlayerId = data;
+        retrievePlayerStats(this.currentPlayerId);
       });
+    },
+    methods: {
+      retrievePlayerStats: function(playerId) {
+        const url = `https://api.mysportsfeeds.com/v1.2/pull/mlb/2019-regular/cumulative_player_stats.json?player=`;
+        const params = {
+          playerstats: "AB,H,HR,RBI,AVG",
+          force: true
+        };
+        axios({
+          method: "get",
+          headers: {
+            Authorization:
+              "Basic NzAxMzNkMmEtNzVmMi00MjdiLWI5ZDYtOTgyZTFhOnNwb3J0c2ZlZWRzMjAxOA=="
+          },
+          url: url + playerId,
+          params: params
+        }).then(response => {
+          console.log(response);
+        });
+      }
     },
     template: `
     <tr class="d-flex" v-if="this.boxGameScore">
