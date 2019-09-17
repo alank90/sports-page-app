6cad969e24153bfa39e06d792ed85db432ccf38f
Alan K <akillian@outlook.com>
2019-09-17 14:15:56 -0400

    Fixed NFL Dates

--- src/js/modules/getScores.js
+++ src/js/modules/getScores.js
@@ -1,11 +1,13 @@
 // /src/js/modules/getScores.js
 
 const axios = require("axios");
+const today = require("../modules/todayDate")
 /* jshint ignore:start */
 
 module.exports = async function(gameday, config) {
+ 
   return (await axios.get(
-    `https://api.mysportsfeeds.com/v1.2/pull/nfl/2018-regular/scoreboard.json?fordate=${gameday}`,
+    `https://api.mysportsfeeds.com/v1.2/pull/nfl/${today.year}-regular/scoreboard.json?fordate=${gameday}`,
     config
   )).data.scoreboard.gameScore;
 };
