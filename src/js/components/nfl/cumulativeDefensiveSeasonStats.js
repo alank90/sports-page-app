fd0b7390281f85e8d00036db654f9ec12e728519
Alan K <akillian@outlook.com>
2019-10-25 14:23:38 -0400

    commit

---
+++ src/js/components/nfl/cumulativeDefensiveSeasonStats.js
@@ -0,0 +1,114 @@
+const Vue = require("vue");
+const axios = require("axios");
+
+const { EventBus } = require("../../modules/event-bus");
+
+const defensiveCumulativeStats = {
+  cumulativeStats: Vue.component("defensive-season-stats", {
+    props: ["props_player_id"],
+    data: function() {
+      return {
+        totalTackles: "",
+        soloTackles: "",
+        sacks: "",
+        interceptions: "",
+        forcedFumbles: "",
+        stuffs: "",
+        showComponent: false,
+        loading: false
+      };
+    },
+    mounted: function() {
+      EventBus.$on("showDefensiveTemplateClicked", this.onShowDefensiveTemplateClicked);
+    },
+    methods: {
+      onShowDefensiveTemplateClicked: function(playerId) {
+        if (playerId === this.props_player_id) {
+          console.log(playerId);
+          this.loading = true;
+          this.showComponent = !this.showComponent;
+          this.retrieveDefensiveStats(playerId);
+        }
+      },
+      retrieveDefensiveStats: function(playerId) {
+        const url = `https://api.mysportsfeeds.com/v1.2/pull/nfl/2019-regular/cumulative_player_stats.json?player=`;
+        const params = {
+          playerstats: "Total,Solo,Sacks,Interceptions,Forced,Stuffs",
+          force: true
+        };
+
+        axios({
+          method: "get",
+          headers: {
+            Authorization:
+              "Basic NzAxMzNkMmEtNzVmMi00MjdiLWI5ZDYtOTgyZTFhOnNwb3J0c2ZlZWRzMjAxOA=="
+          },
+          url: url + playerId,
+          params: params
+        }).then(response => {
+          this.totalTackles =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.Total[
+              "#text"
+            ];
+          this.soloTackles =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.Solo[
+              "#text"
+            ];
+          this.sacks =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.Sacks[
+              "#text"
+            ];
+          this.interceptions =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.Interceptions[
+              "#text"
+            ];
+          this.forcedFumbles =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.Forced[
+              "#text"
+            ];
+          this.stuffs =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.Stuffs[
+              "#text"
+            ];
+
+          this.loading = false;
+        });
+      }
+    },
+    template: `
+          <div v-if="showComponent">
+            <transition name="fade">
+              <thead class="d-flex flex-wrap">
+                <th class="col-12 stats-header"> Season Stats </th>
+                  <th class="col-2 justify-content-center season-stats-headers" scope="col">TT</th>
+                  <th class="col-2 justify-content-center season-stats-headers" scope="col">Solo</th>
+                  <th class="col-2 justify-content-center season-stats-headers" scope="col">Sks</th>
+                  <th class="col-2 justify-content-center season-stats-headers" scope="col">Int</th>
+                  <th class="col-2 justify-content-center season-stats-headers" scope="col">F</th>
+                  <th class="col-2 justify-content-right season-stats-headers" scope="col">Stff</th>
+              </thead>
+            </transition>
+  
+            <transition name="fade">
+              <tr class="d-flex flex-wrap">
+                  <span class="cumlativeStatsLoading" v-if="loading">
+                  Loading
+                  <!-- below is our font awesome icon with the class “spin-it” where 
+                      we have set the infinite animation:                        -->
+                    <i class="fas fa-cog spin-it fa-sm" aria-hidden="true"></i>
+                  </span>
+                  <td class="col-2 justify-content-center season-stats" justify-content="center">
+                  {{ totalTackles }}</td>
+                  <td class="col-2 justify-content-center season-stats">{{ solo }}</td>
+                  <td class="col-2 justify-content-center season-stats"> {{ sacks }}</td>
+                  <td class="col-2 justify-content-center season-stats"> {{ interceptions }}</td>
+                  <td class="col-2 justify-content-center season-stats"> {{ forcedFumbles }}</td>
+                  <td class="col-2 justify-content-center season-stats">{{ stuffs }}</td>
+              </tr>
+            </transition>
+          </div>
+      `
+  })
+};
+
+module.export = { defensiveCumulativeStats };
---
+++ src/js/components/nfl/cumullativeRushingSeasonStats.js
@@ -0,0 +1,117 @@
+const Vue = require("vue");
+const axios = require("axios");
+
+const { EventBus } = require("../../modules/event-bus");
+
+const rushingCumulativeStats = {
+  cumulativeStats: Vue.component("rushing-season-stats", {
+    props: ["props_player_id"],
+    data: function() {
+      return {
+        rushAttempts: "",
+        rushYards: "",
+        rushAvg: "",
+        rushTD: "",
+        rushLong: "",
+        receptions: "",
+        showComponent: false,
+        loading: false
+      };
+    },
+    mounted: function() {
+      EventBus.$on(
+        "showRushingTemplateClicked",
+        this.onShowRushingTemplateClicked
+      );
+    },
+    methods: {
+      onShowRushingTemplateClicked: function(playerId) {
+        if (playerId === this.props_player_id) {
+          console.log(playerId);
+          this.loading = true;
+          this.showComponent = !this.showComponent;
+          this.retrieveRushingStats(playerId);
+        }
+      },
+      retrieveRushingStats: function(playerId) {
+        const url = `https://api.mysportsfeeds.com/v1.2/pull/nfl/2019-regular/cumulative_player_stats.json?player=`;
+        const params = {
+          playerstats: "Att,Pct,Yds,Int,TD,Avg,Lng,Rec",
+          force: true
+        };
+
+        axios({
+          method: "get",
+          headers: {
+            Authorization:
+              "Basic NzAxMzNkMmEtNzVmMi00MjdiLWI5ZDYtOTgyZTFhOnNwb3J0c2ZlZWRzMjAxOA=="
+          },
+          url: url + playerId,
+          params: params
+        }).then(response => {
+          this.rushAttempts =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.RushAttempts[
+              "#text"
+            ];
+          this.rushYards =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.RushYards[
+              "#text"
+            ];
+          this.rushAvg =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.RushAverage[
+              "#text"
+            ];
+          this.rushTD =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.RushTD[
+              "#text"
+            ];
+          this.rushLong =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.RushLng[
+              "#text"
+            ];
+          this.receptions =
+            response.data.cumulativeplayerstats.playerstatsentry[0].stats.Receptions[
+              "#text"
+            ];
+
+          this.loading = false;
+        });
+      }
+    },
+    template: `
+        <div v-if="showComponent">
+          <transition name="fade">
+            <thead class="d-flex flex-wrap">
+              <th class="col-12 stats-header"> Season Stats </th>
+                <th class="col-2 justify-content-center season-stats-headers" scope="col">Att</th>
+                <th class="col-2 justify-content-center season-stats-headers" scope="col">Yds</th>
+                <th class="col-2 justify-content-center season-stats-headers" scope="col">Avg</th>
+                <th class="col-2 justify-content-center season-stats-headers" scope="col">TD</th>
+                <th class="col-2 justify-content-center season-stats-headers" scope="col">Lng</th>
+                <th class="col-2 justify-content-right season-stats-headers" scope="col">Rec</th>
+            </thead>
+          </transition>
+
+          <transition name="fade">
+            <tr class="d-flex flex-wrap">
+                <span class="cumlativeStatsLoading" v-if="loading">
+                Loading
+                <!-- below is our font awesome icon with the class “spin-it” where 
+                    we have set the infinite animation:                        -->
+                  <i class="fas fa-cog spin-it fa-sm" aria-hidden="true"></i>
+                </span>
+                <td class="col-2 justify-content-center season-stats" justify-content="center">
+                {{ rushAttempts }} </td>
+                <td class="col-2 justify-content-center season-stats">{{ rushYards }}</td>
+                <td class="col-2 justify-content-center season-stats"> {{ rushAvg }}</td>
+                <td class="col-2 justify-content-center season-stats"> {{ rushTD }}</td>
+                <td class="col-2 justify-content-center season-stats"> {{ rushLong }}</td>
+                <td class="col-2 justify-content-center season-stats">{{ receptions }}</td>
+            </tr>
+          </transition>
+        </div>
+    `
+  })
+};
+
+module.export = { rushingCumulativeStats };
