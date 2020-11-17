80e682c445ab9cab8266ca75d959b705201fba07
Alan K <akillian@outlook.com>
2020-11-17 14:26:27 -0500

    commit

--- src/js/components/nfl/nflLeagueLeaders.js
+++ src/js/components/nfl/nflLeagueLeaders.js
@@ -10,7 +10,31 @@ const leagueLeaders = {
     data: function () {
       return {
         test: "testing",
-      };
+        fetchURL:
+          "https://api.mysportsfeeds.com/v1.2/pull/nfl/2020-2021-regular/cumulative_player_stats.json",
+          params = {
+            teamstats: "none",
+            playerstats:
+              "Att,Comp,Yds,Rec,TD",
+            position: "qb",
+            sort: "stats.Yds",
+            limit: 10,
+            force: true,
+          }
+      
+        };
+    },
+    methods: {
+      retrieveTopOffensivePlayers: function () {
+        fetch(fetchURL, {
+          method: "get",
+          headers: {
+            Authorization:
+              "Basic NzAxMzNkMmEtNzVmMi00MjdiLWI5ZDYtOTgyZTFhOnNwb3J0c2ZlZWRzMjAxOA==",
+          },
+          params: this.params,
+        });
+      },
     },
   }),
 };
