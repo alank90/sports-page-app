71c3b77d80611263326f9740cf66de017b1c0c24
Alan Killian <akillian@outlook.com>
2021-01-08 20:40:30 -0500

    commit

--- scripts/build.js
+++ scripts/build.js
@@ -191,49 +191,28 @@ require("rimraf")("./dist", function () {
       const miscOperations = async function (result) {
         console.log(result);
         try {
-          // Copy CNAME to /dist folder
-          fs.stat("CNAME", async (error) => {
-            if (error) {
-              console.log(`No CNAME file present ${warning}`);
-            } else {
-              await copyFile("CNAME", "dist/CNAME", (err) => {
-                if (err) {
-                  console.log("Error Found:", err);
-                } else {
-                  console.log(`Copied CNAME to /dist. ${checkMark}`);
-                }
-              });
-            }
-          });
+          // Copy CNAME,favicon.jpg, and 200.html to /dist folder
+          await copyFile("CNAME", "./dist/CNAME")
+            .then(() =>
+              console.log(`CNAME copied successfully to /dist! ${checkMark}`)
+            )
+            .catch(() => console.log(`No CNAME file to copy. ${warning}`));
 
-          fs.stat("favicon.jpg", async (error) => {
-            if (error) {
-              console.log(`No favicon.jpg file present ${warning}`);
-            } else {
-              await copyFile("favicon.jpg", "dist/favicon.jpg", (err) => {
-                if (err) {
-                  console.log("Error Found:", err);
-                } else {
-                  console.log(`Copied favicon.jpg to /dist. ${checkMark}`);
-                }
-              });
-            }
-          });
+          await copyFile("favicon.jpg", "./dist/favicon.jpg")
+            .then(() =>
+              console.log(
+                `favicon.jpg successfully copied to /dist! ${checkMark}`
+              )
+            )
+            .catch(() =>
+              console.log(`No favicon.jpg file to copy. ${warning}`)
+            );
 
-          // Copy 200.html to /dist folder
-          fs.stat("200.html", async (error) => {
-            if (error) {
-              console.log(`No 200.html file present ${warning}`);
-            } else {
-              await copyFile("200.html", "dist/200.html", (err) => {
-                if (err) {
-                  console.log("Error Found:", err);
-                } else {
-                  console.log(`Copied 200.html to /dist. ${checkMark}`);
-                }
-              });
-            }
-          });
+          await copyFile("200.html", "./dist/200.html")
+            .then(() =>
+              console.log(`200.html copied successfully to /dist! ${checkMark}`)
+            )
+            .catch(() => console.log(`No 200.html file to copy. ${warning}`));
 
           // Copy /src/resources to /dist folder
           const readDirectory = await readdir("./resources");
